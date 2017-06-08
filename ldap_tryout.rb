a3c7c8a78a0096f600f51701b127faf769510a5a
B Kleinen <kleinen@infrastructure.de>
2017-06-08 22:34:04 +0200

    cleanup

--- ldap_tryout.rb
+++ ldap_tryout.rb
@@ -1,27 +1,32 @@
 require 'net/ldap'
 
-ldap = Net::LDAP.new(host: "141.45.146.101", port: 389)
-
-#auth = ldap.bind(method: :simple, username: "uid=kleinen, ou=Users, o=f4, dc=htw-berlin, dc=de", password: "#{ENV["PW"]}")
-#auth = ldap.bind(method: :simple, username: "uid=kleinen, ou=Users, o=f4, dc=htw-berlin, dc=de", password: "#{ENV["PW"]}")
-auth = 9
-puts "authorization successful: #{auth} (#{auth.class})"
 
 
-# serach
-treebase = "o=f4, dc=htw-berlin, dc=de"
-f = Net::LDAP::Filter.eq( "uid", "kleinen" )
+def try_auth
+  ldap = Net::LDAP.new(host: "141.45.146.101", port: 389)
+  puts "export PW=<your passwd> to test " unless ENV["PW"]
+  auth = ldap.bind(method: :simple, username: "uid=kleinen, ou=Users, o=f4, dc=htw-berlin, dc=de", password: "#{ENV["PW"]}")
+  puts "authorization successful: #{auth} (#{auth.class})"
+end
 
-r = ldap.search(:base => treebase, :filter => f)
+def try_search
+  ldap = Net::LDAP.new(host: "141.45.146.101", port: 389)
+  # search
+  treebase = "o=f4, dc=htw-berlin, dc=de"
+  f = Net::LDAP::Filter.eq( "uid", "kleinen" )
 
-# Net::LDAP::Entry
+  r = ldap.search(:base => treebase, :filter => f)
 
-puts r.size
-e = r.first
-puts e.inspect
+  # Net::LDAP::Entry
 
-puts e.uid
-puts e.objectclass
+  puts r.size
+  e = r.first
+  puts e.inspect
+
+  puts e.uid
+  puts e.objectclass
+end
+
 
  # filter = Net::LDAP::Filter.eq("cn", "*") -> formatierungsfehler
 
@@ -33,9 +38,17 @@ puts e.objectclass
 # r = ldap.search(:base => treebase, :filter => f)
 # puts r.map(&:uid).inspect
 
+def try_search2
+  ldap = Net::LDAP.new(host: "141.45.146.101", port: 389)
+  treebase = "o=f4, dc=htw-berlin, dc=de"
+  f = Net::LDAP::Filter.eq( "uid", "*" )
+  r = ldap.search(:base => treebase, :filter => f)
+  puts ldap.get_operation_result
+  puts r.map(&:uid).inspect
+  puts r.first
+  puts r.size
+end
 
-treebase = "o=f4, dc=htw-berlin, dc=de"
-f = Net::LDAP::Filter.eq( "uid", "*" )
-r = ldap.search(:base => treebase, :filter => f)
-puts ldap.get_operation_result
-puts r.map(&:uid).inspect
+#try_auth
+#try_search
+try_search2
